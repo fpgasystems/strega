4c2b876b776c730e00a1fe20d1c6636c44581ee0
Fabio Maschi <fabio.maschi@inf.ethz.ch>
2023-01-15 14:08:11 +0100

    fix mock host to health check

--- hw/mock/host.cc
+++ hw/mock/host.cc
@@ -71,97 +71,27 @@ int main (int argc, char **argv) {
   // APP KERNEL
   ///////////////////////////////////////////////////////////////////////////////////////
 
-  cl::Kernel user_kernel;
-  OCL_CHECK(err, user_kernel = cl::Kernel(program, "static_pages", &err));
+  // cl::Kernel user_kernel;
+  // OCL_CHECK(err, user_kernel = cl::Kernel(program, "static_pages", &err));
 
-  uint32_t header_length;
-  uint32_t body_pointer;
-  uint32_t body_length;
+  // uint32_t header_length;
+  // uint32_t body_pointer;
+  // uint32_t body_length;
 
-  // Headers
-  std::string headers_path = argv[ARGV_HEADERS];
-  std::vector<char, aligned_allocator<char>>* headers_data;
-  std::ifstream file(headers_path, std::ios::in | std::ios::binary);
-  if (file.is_open()) {
-    file.seekg(0, std::ios::end);
-    header_length = file.tellg();
-    std::cout << "headers size: " << header_length << " bytes" << std::endl;
+  // uint32_t arg = 6;
+  // std::cout << "arg[" << arg << "] = " << header_length << std::endl;
+  // OCL_CHECK(err, err = user_kernel.setArg(arg++, header_length));
+  // std::cout << "arg[" << arg << "] = " << body_length << std::endl;
+  // OCL_CHECK(err, err = user_kernel.setArg(arg++, body_length));
 
-    char* file_buffer = new char[header_length];
-    file.seekg(0, std::ios::beg);
-    file.read(file_buffer, header_length);
+  // // Launch the Kernel
+  // std::cout << "enqueue user kernel..." << std::endl;
 
-    headers_data = new std::vector<char, aligned_allocator<char>>(header_length);
-    memcpy(headers_data->data(), file_buffer, header_length);
-    file.close();
-    delete [] file_buffer;
-  } else {
-    std::cerr << "[!] Failed to open HEADERS.txt file" << std::endl;
-    exit(EXIT_FAILURE);
-  }
+  // OCL_CHECK(err, err = q.enqueueTask(user_kernel));
+  // OCL_CHECK(err, err = q.finish())
 
-  // Body
-  std::string body_path = argv[ARGV_BODY];
-  std::vector<char, aligned_allocator<char>>* body_data;
-  file = std::ifstream(body_path, std::ios::in | std::ios::binary);
-  if (file.is_open()) {
-    file.seekg(0, std::ios::end);
-    body_length = file.tellg();
-    std::cout << "body size: " << body_length << " bytes" << std::endl;
-
-    char* file_buffer = new char[body_length];
-    file.seekg(0, std::ios::beg);
-    file.read(file_buffer, body_length);
-
-    body_data = new std::vector<char, aligned_allocator<char>>(body_length);
-    memcpy(body_data->data(), file_buffer, body_length);
-    file.close();
-    delete [] file_buffer;
-  } else {
-    std::cerr << "[!] Failed to open BODY.txt file" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  // std::vector<char, aligned_allocator<char>> buffff(header_length + body_length, 0);
-  // OCL_CHECK(err,
-  //     cl::Buffer buffer_device(context,
-  //       CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-  //       header_length + body_length,
-  //       buffff.data(),
-  //       &err));
-
-  uint32_t arg = 6;
-  body_pointer = header_length;
-  // std::cout << "arg[" << arg << "] = " << &buffer_device << std::endl;
-  // OCL_CHECK(err, err = user_kernel.setArg(arg++, buffer_device));
-  std::cout << "arg[" << arg << "] = " << header_length << std::endl;
-  OCL_CHECK(err, err = user_kernel.setArg(arg++, header_length));
-  std::cout << "arg[" << arg << "] = " << body_pointer << std::endl;
-  OCL_CHECK(err, err = user_kernel.setArg(arg++, body_pointer));
-  std::cout << "arg[" << arg << "] = " << body_length << std::endl;
-  OCL_CHECK(err, err = user_kernel.setArg(arg++, body_length));
-
-  // cl::Event blocking_call_event;
-  // OCL_CHECK(err,
-  //   err = q.enqueueWriteBuffer(buffer_device,          // buffer on the FPGA
-  //                               CL_TRUE,               // blocking call
-  //                               0,                     // buffer offset in bytes
-  //                               header_length,         // Size in bytes
-  //                               headers_data->data(),  // Pointer to the data to copy
-  //                               nullptr, &blocking_call_event));
-  // OCL_CHECK(err,
-  //   err = q.enqueueWriteBuffer(buffer_device,          // buffer on the FPGA
-  //                               CL_TRUE,               // blocking call
-  //                               header_length,         // buffer offset in bytes
-  //                               body_length,           // Size in bytes
-  //                               body_data->data(),     // Pointer to the data to copy
-  //                               nullptr, &blocking_call_event));  
-
-  // Launch the Kernel
-  std::cout << "enqueue user kernel..." << std::endl;
-
-  OCL_CHECK(err, err = q.enqueueTask(user_kernel));
-  OCL_CHECK(err, err = q.finish())
+  std::cout << "Only autorun kernels in this mock. Press ENTER to finish the simulation..." << std::endl;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
   std::cout << "finished" << std::endl;
 }
